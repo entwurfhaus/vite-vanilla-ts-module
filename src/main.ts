517477433fa9c530325ae848cd322cd3f73afce3
Brian Ng <brianngchongeu@gmail.com>
2022-06-04 12:13:46 +1000

    fix: import new dateTimeHelper

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { add, subtract, divide, multiply } from "./utils/math";
 import { hello } from "./utils/hello";
+import { dateTimeHelper } from "./utils/dateTime";
 
-export { add, subtract, divide, multiply, hello };
+export { add, subtract, divide, multiply, hello, dateTimeHelper };
